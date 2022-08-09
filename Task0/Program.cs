568547327f2b8cd07396ad38a91040f55c33d672
Beslan06 <bor.mm2011@yandex.ru>
2022-08-09 17:06:33 +0300

    Домашняя работа по C# 3

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -13,32 +13,3 @@ else
     Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
 
 
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-    
-    Console.WriteLine("Введите любое число:");
-       num = Convert.ToInt32(Console.ReadLine());
-          if (num % 2 == 1)
-    Console.WriteLine("Число " + num + " является: нечётным");
-          else
-    Console.WriteLine("Число " + num + "является: чётным");
-
-
-// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
-int i = 1;
-bool not = true;
-
-Console.WriteLine("Введите число:");
-num = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Чётные числа от 1 до " + num);
-    while (i <= num)
-    if (i % 2 != 1)
-Console.Write(i + ", ");
-not = false;
-i++;
-    if (not)
-Console.WriteLine("Нет чётных чисел!");
-default:
-begin = false;
-
