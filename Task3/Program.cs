02c8de3d9e7f42e4db9431bde028833eb93434c8
Beslan06 <bor.mm2011@yandex.ru>
2022-08-09 18:15:32 +0300

    Домашняя работа по C# 4

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = 2;
+int even = 2;
+
+while (num >= remain)
+while (num >= even)
+{
+    Console.Write(remain + " ");
+    remain = remain + 2;
+    Console.Write(even + " ");
+    even = even + 2;
+}
