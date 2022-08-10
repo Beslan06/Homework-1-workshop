d4d8af4d88859977fb430fec62440065a67ee205
Beslan06 <bor.mm2011@yandex.ru>
2022-08-10 14:06:17 +0300

    исправление ошибки

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
             Console.WriteLine("Введите число:");
-            num = Convert.ToInt32(Console.ReadLine());
+            int num = Convert.ToInt32(Console.ReadLine());
 
             if (num % 2 == 1)
                 Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
